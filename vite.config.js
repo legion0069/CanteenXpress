4e2bc0d81e4553289fd938b0521e20d10b906050
legion0069 <152319571+legion0069@users.noreply.github.com>
2025-03-03 23:11:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0', // Ensures Render detects the port
-    port: process.env.PORT || 3000, // Uses Render-assigned port
+    port: parseInt(process.env.PORT) || 3000, // Uses Render-assigned port
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
