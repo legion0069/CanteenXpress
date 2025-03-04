29d27edc26dbe0f93e4bbebcbac73776e004486f
legion0069 <152319571+legion0069@users.noreply.github.com>
2025-03-04 18:12:13 +0530

    Update vite.config.js

--- Project/vite.config.js
+++ Project/vite.config.js
@@ -1,24 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'public',
-    emptyOutDir: true,// Changed from 'dist' to 'public' for Vercel
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   server: {
-    host: true, // Allows access from any network
-    port: process.env.PORT || 3000, // Uses environment variable or defaults to 3000
-    strictPort: true, // Ensures it fails if the port is unavailable
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
         changeOrigin: true,
-      },
-    },
-  },
+      }
+    }
+  }
 });
