58cffa01b1977203a4bd713c9a8caa02510467eb
legion0069 <152319571+legion0069@users.noreply.github.com>
2025-03-04 17:57:32 +0530

    Update vite.config.js

--- Project/vite.config.js
+++ Project/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Change from 'dist' to 'public' for Vercel
+    outDir: 'public', // Changed from 'dist' to 'public' for Vercel
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
